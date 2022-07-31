db17e2496b193a117a773fd216cf251ea88a68fb
KHC <98343091+HaleyKwok@users.noreply.github.com>
2022-07-31 15:45:33 +0800

    Create PyPy July 31.py

---
+++ PyPy July 31.py
@@ -0,0 +1,13 @@
+import time
+starttime = time.time() 
+
+run = 0 
+for i in range(1, 5000):
+    for j in range(1, 5000):
+        run += i + j
+
+print(f"The result is {run}")
+
+endtime = time.time() 
+print(f"It took {endtime-starttime:.2f} seconds to compute") 
+
