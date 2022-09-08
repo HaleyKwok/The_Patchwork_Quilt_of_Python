c8183c996f1d237730635d494fb700aef2a218d6
KHC <98343091+HaleyKwok@users.noreply.github.com>
2022-09-08 21:51:54 +0800

    Update wifipwd.py

--- wifipwd.py
+++ wifipwd.py
@@ -1,5 +1,9 @@
-import office
+# import office
 
 
-for len_pwd in range(1, 5000):
-    office.tools.pwd4wifi(len_pwd=len_pwd, pwd_list=['12345678', '87654321', 'qwertyui', 'asdfghjk', 'zxcvbnm'])
+# for len_pwd in range(1, 5000):
+#     office.tools.pwd4wifi(len_pwd=len_pwd, pwd_list=['12345678', '87654321', 'qwertyui', 'asdfghjk', 'zxcvbnm'])
+
+# no module name as win32com
+
+# only for windows
